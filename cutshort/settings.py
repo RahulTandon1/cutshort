2971f18c1690129da03e7a13a2c26ce81be3ec50
Rahul Tandon <jesuisrahult@gmail.com>
2020-08-29 11:36:30 +0530

    static stuff

--- cutshort/settings.py
+++ cutshort/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         # 'PASSWORD': '1',
         # 'HOST': '127.0.0.1',
         # 'PORT': '5432',
-        'ENGINE' = 'django.db.backends.sqlite3',
-        'NAME' = os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 # db_from_env = dj_database_url.config(conn_max_age=600)
