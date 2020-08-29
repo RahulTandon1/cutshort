c1a24778b7ab3e64b55c088bce7f6021b9586181
Rahul Tandon <jesuisrahult@gmail.com>
2020-08-29 11:58:22 +0530

    static stuff

--- cutshort/settings.py
+++ cutshort/settings.py
@@ -130,3 +130,4 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
